39ee91f5093e55b1d020ba31f3ad4f90f559647d
Nathan Hamilton <nathan.a.hamilton@gmail.com>
2021-09-22 11:12:57 -0500

    fix: print statements for each column header on monster.csv

--- csvReader.py
+++ csvReader.py
@@ -1,7 +1,22 @@
 import csv
+from os import times
 
 with open('monster.csv', 'r') as monsterCSV:
     monsterReader = csv.reader(monsterCSV)
 
+    # next(monsterReader)
+
     for line in monsterReader:
+        timestamp = line[0]
+        researcher = line[1]
+        monster = line[2]
+        latitude = line[3]
+        longitude = line[4]
+
+        # print(timestamp)
+        # print(researcher)
+        # print(monster)
+        # print(latitude)
+        # print(longitude)
+
         print(line)
